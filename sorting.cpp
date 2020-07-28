cd5e87faa8203367d3fc4b6019d7eecd795c17f1
Tanish Dhanjal <35321932+TANISH-18@users.noreply.github.com>
2020-07-28 11:15:54 +0530

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -3,10 +3,7 @@
 #include <bits/stdc++.h> 
 using namespace std; 
   
-/* This function partitions a[] in three parts 
-   a) a[l..i] contains all elements smaller than pivot 
-   b) a[i+1..j-1] contains all occurrences of pivot 
-   c) a[j..r] contains all elements greater than pivot */
+
 void partition(int a[], int l, int r, int &i, int &j) 
 { 
     i = l-1, j = r; 
@@ -15,33 +12,27 @@ void partition(int a[], int l, int r, int &i, int &j)
   
     while (true) 
     { 
-        // From left, find the first element greater than 
-        // or equal to v. This loop will definitely terminate 
-        // as v is last element 
+       
         while (a[++i] < v); 
   
-        // From right, find the first element smaller than or 
-        // equal to v 
+        
         while (v < a[--j]) 
             if (j == l) 
                 break; 
-  
-        // If i and j cross, then we are done 
+   
         if (i >= j) break; 
   
-        // Swap, so that smaller goes on left greater goes on right 
+       
         swap(a[i], a[j]); 
   
-        // Move all same left occurrence of pivot to beginning of 
-        // array and keep count using p 
+      
         if (a[i] == v) 
         { 
             p++; 
             swap(a[p], a[i]); 
         } 
   
-        // Move all same right occurrence of pivot to end of array 
-        // and keep count using q 
+       
         if (a[j] == v) 
         { 
             q--; 
@@ -49,30 +40,28 @@ void partition(int a[], int l, int r, int &i, int &j)
         } 
     } 
   
-    // Move pivot element to its correct index 
+
     swap(a[i], a[r]); 
   
-    // Move all left same occurrences from beginning 
-    // to adjacent to arr[i] 
+ 
     j = i-1; 
     for (int k = l; k < p; k++, j--) 
         swap(a[k], a[j]); 
   
-    // Move all right same occurrences from end 
-    // to adjacent to arr[i] 
+     
     i = i+1; 
     for (int k = r-1; k > q; k--, i++) 
         swap(a[i], a[k]); 
 } 
   
-// 3-way partition based quick sort 
+ 
 void quicksort(int a[], int l, int r) 
 { 
     if (r <= l) return; 
   
     int i, j; 
   
-    // Note that i and j are passed as reference 
+ 
     partition(a, l, r, i, j); 
   
     // Recur 
@@ -80,7 +69,6 @@ void quicksort(int a[], int l, int r)
     quicksort(a, i, r); 
 } 
   
-// A utility function to print an array 
 void printarr(int a[], int n) 
 { 
     for (int i = 0; i < n; ++i) 
@@ -88,7 +76,7 @@ void printarr(int a[], int n)
     printf("\n"); 
 } 
   
-// Driver program 
+
 int main() 
 { 
     int size;
